5472e32d02b196809d484c473d01369cdbf9fdbc
samyunwei <259375702@qq.com>
2016-07-20 00:13:43 +0800

    add expression

---
+++ expression.lua
@@ -0,0 +1,86 @@
+#!/usr/bin/env lua
+--# File Name: expression.lua
+--# Author: Sam
+--# mail: samyunwei@163.com
+--# Created Time: 二  7/19 23:44:47 2016
+--#########################################################################
+print('P19')
+x = math.pi
+print(x - x%0.01)
+
+function isturnback(angle)
+    local tolerance = 10
+    angle = angle % 360
+    return (math.abs(angle - 180) < tolerance)
+end
+print(isturnback(-180))
+
+print("P21")
+
+print(4 and 5)
+print(nil and 13)
+print(false and 13)
+print(4 or 5)
+print(false or 5)
+
+print(not nil)
+print(not false)
+print(not 0)
+print(not not nil)
+
+print('P22')
+
+print('Hello' .. 'World')
+print(0 .. 1)
+
+a = "Hello"
+print(a.. 'World')
+print(a)
+
+print('P23')
+
+w = {x = 0,y=0,label = "console"}
+x = {math.sin(0),math.sin(1),math.sin(2)}
+w[1] = "anther filed"
+x.f = w
+print(w["x"])
+print(w[1])
+print(x.f[1])
+w.x = nil
+
+
+print('P24')
+
+polyline = {
+    color = "blue",thickness = 2,npoints = 4,
+    {
+        x = 0,y = 0
+    },
+    {
+        x = -10,y = 0
+    },
+    {
+        x = -10,y = 1
+    },
+    {
+        x = 0,y = 1
+    }
+}
+
+print(polyline[2].x)
+print(polyline[4].y)
+
+opnames = {
+    ["+"] = "add",["-"] = "sub",["*"] = "mul",["/"] = "div"
+}
+
+i = 20;s = "-"
+
+a = {
+    [i+0] = s,[i+1] = s ..s,[i+2] = s..s..s
+}
+
+print(opnames[s])
+print(a[22])
+
+
