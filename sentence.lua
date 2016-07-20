41da2a73970cfe43f1dbba1ea5046a76bcd0fd92
samyunwei <259375702@qq.com>
2016-07-21 00:17:58 +0800

    add sentence

---
+++ sentence.lua
@@ -0,0 +1,19 @@
+#!/usr/bin/env lua
+--# File Name: sentence.lua
+--# Author: Sam
+--# mail: samyunwei@163.com
+--# Created Time: 三  7/20 23:23:26 2016
+--#########################################################################
+print('page 27')
+x = 2
+y = 3
+print(x..y)
+x,y = y,x
+print(x..y)
+
+a,b,c = 0,1
+print(a,b,c)
+a,b = a+1,b+1,b+2
+print(a,b)
+a,b,c = 0,0,0
+print(a,b,c)
