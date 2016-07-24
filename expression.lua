85b3aa9bbec0f8d94a45e0cdc79b11172739a271
samyunwei <259375702@qq.com>
2016-07-25 01:45:44 +0800

    add

--- expression.lua
+++ expression.lua
@@ -84,3 +84,32 @@ print(opnames[s])
 print(a[22])
 
 
+print("P28")
+x = 10
+local i = 1
+
+while i <= x do
+    local x = i*2
+    print(x)
+    i = i+1
+end
+
+
+if i > 20 then
+    local x
+    x = 20
+    print(x+2)
+else
+    print(x)
+end
+
+print(x)
+
+print("P29")
+local a,b = 1,10
+if a < b then
+    print(a)
+    local a
+    print(a)
+end
+print(a,b)
