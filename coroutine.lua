a0ca39677866ee7529e175667f87a6cdf7029585
samyunwei <259375702@qq.com>
2016-08-26 00:22:03 +0800

    8.26

--- coroutine.lua
+++ coroutine.lua
@@ -55,7 +55,7 @@ end)
 print(coroutine.resume(co))
 
 --]]
-
+--[[
 function receive(prod)
     local status,value = coroutine.resume(prod)
     return value
@@ -98,3 +98,66 @@ end
 p = producer()
 f = filter(p)
 consumer(f)
+--]]
+
+--[[
+function printResult(a)
+    for i = 1,#a do
+        io.write(a[i]," ")
+    end
+    io.write("\n")
+end
+
+function permgen(a,n)
+    n = n or #a
+    if n <= 1 then 
+        printResult(a)
+    else
+        for i = 1,n do
+            a[n],a[i] = a[i],a[n]
+            permgen(a,n-1)
+            a[n],a[i] = a[i],a[n]
+        end
+    end
+end
+
+permgen({
+    1,2,3,4
+})
+--]]
+
+function permgen(a,n)
+    n = n or #a
+    if n <= 1 then 
+       coroutine.yield(a) 
+    else
+        for i = 1,n do
+            a[n],a[i] = a[i],a[n]
+            permgen(a,n-1)
+            a[n],a[i] = a[i],a[n]
+        end
+    end
+end
+--[[
+function permutations(a)
+    local co = coroutine.create(function() permgen(a) end)
+    return function() 
+        local code,res = coroutine.resume(co)
+        return res
+    end
+end
+--]]
+function permutations (a)
+    return coroutine.wrap(function() permgen(a) end)
+end
+function printResult(a)
+    for i = 1,#a do
+        io.write(a[i]," ")
+    end
+    io.write("\n")
+end
+
+
+for p in permutations{"a","b","c"} do
+    printResult(p)
+end
