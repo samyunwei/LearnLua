418cb92784dc0f257552b49459ae3cd33a8f3ba1
samyunwei <259375702@qq.com>
2016-09-12 23:43:17 +0800

    9.12

--- metatableandmetamethod.lua
+++ metatableandmetamethod.lua
@@ -74,7 +74,22 @@ function Set.print(s)
     print(Set.toString(s))
 end
 
+function Set.le(a,b)
+    for k in pairs(a) do
+        if not b[k] then return false end
+    end
+    return true
+end
 
+function Set.lt(a,b)
+    return Set.le(a,b) and not Set.le(b,a)
+end
+
+function Set.eq(a,b)
+    return Set.le(a,b) and Set.le(b,a)
+end
+
+--[[
 s1 = Set.new{
     10,20,30,50
 }
@@ -86,11 +101,53 @@ s2 = Set.new{
 print(getmetatable(s1))
 print(getmetatable(s2))
 
-
+--]]
 mt.__add = Set.union
 
-s3 = s1 + s2
-Set.print(s3)
+--s3 = s1 + s2
+--Set.print(s3)
 
 mt.__mul = Set.intersection
-Set.print((s1 + s2) * s1)
+--Set.print((s1 + s2) * s1)
+
+
+
+mt.__le = Set.le
+mt.__lt = Set.lt
+mt.__eq = Set.eq
+--[[
+s1 = Set.new{
+    2,4
+}
+s2 = Set.new{
+    4,10,2
+}
+print(s1 <= s2)
+print(s1 < s2)
+print(s1 >= s1)
+print(s1 > s1)
+print(s1 == s2 *s1)
+--]]
+--[[
+print({
+
+})
+mt.__tostring = Set.toString
+
+s1 = Set.new{
+    10,4,5
+}
+print(s1)
+
+--]]
+
+mt.__metatable = "not your business"
+
+s1 = Set.new{
+
+}
+print(getmetatable(s1))
+setmetatable(s1,{
+
+})
+
