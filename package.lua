a712fc663e5ad746fea33f95b4d321a3fe79c46e
samyunwei <259375702@qq.com>
2016-09-26 00:03:31 +0800

    package fin

--- package.lua
+++ package.lua
@@ -4,13 +4,77 @@
 --# mail: samyunwei@163.com
 --# Created Time: 六  9/24 11:11:31 2016
 --#########################################################################
-complex = {
+--[[
+local modname = "complex"
+local M = {
 
 }
+_ENV[modname] = M
+package.loaded[modname] = M
 
-function complex.new(r,i) return {r = r, i = i}
+setmetatable(M,{
+    __index = _ENV
+})
+
+local sqrt = math.sqrt
+local io =  io
+local _ENV = M
+
+
+
+function M.new(r,i) 
+    return {r = r, i = i}
 end
 
-complex.i = complex.new(0,1)
+M.i = M.new(0,1)
 
- 
+ function M.add(c1,c2)
+     return M.new(c1.r + c2.r,c1.i+c2.i)
+ end
+
+
+
+function M.mul(c1,c2)
+    return M.new(c1.r*c2.r -c1.i*c2.i,c1.r*c2.i + c1.i*c2.r)
+end
+
+local function inv(c)
+    local n = c.r ^ 2 + c.i ^ 2
+    return M.new(c.r/n,-c.i/n)
+end
+
+function M.div(c1,c2)
+    return M(c1,inv(c2))
+end
+
+--]]
+
+module("complex")
+
+function new(r,i) 
+    return {r = r, i = i}
+end
+
+i = new(0,1)
+
+ function add(c1,c2)
+     return new(c1.r + c2.r,c1.i+c2.i)
+ end
+
+
+
+function mul(c1,c2)
+    return new(c1.r*c2.r -c1.i*c2.i,c1.r*c2.i + c1.i*c2.r)
+end
+
+local function inv(c)
+    local n = c.r ^ 2 + c.i ^ 2
+    return new(c.r/n,-c.i/n)
+end
+
+function div(c1,c2)
+    return c1,inv(c2)
+end
+
+
+
