729c8edb5241ee53e4803fbb52f0aafd314ddc36
samyunwei <259375702@qq.com>
2016-09-29 00:43:09 +0800

    more oop

--- oop.lua
+++ oop.lua
@@ -22,6 +22,35 @@ function Account:new(o)
     self.__index = self
     return o
 end
+
+function Account:withdraw(v)
+    if v > self.balance then error"insufficient funds" end
+    self.balance = self.balance - v
+end
+
+SpecialAccount = Account:new()
+s = SpecialAccount:new{
+    limit = 1000.00
+}
+function SpecialAccount:withdraw(v)
+    if v - self.balance >= self.getLimit() then
+        error"insufficient funds"
+    end
+    self.balance = self.balance - v
+end
+
+function SpecialAccount:getLimit()
+    return self.limit or 0
+end
+
+function s:getLimit()
+    return self.balance * 0.10
+end
+
+
+s:deposit(100.00)
+
+
 --[[
 Account.deposit(Account,200.00)
 Account:withdraw(100.00)
