ee1c415e8f25b461fae6160d32e852e557fb9f07
samyunwei <259375702@qq.com>
2016-10-17 00:14:01 +0800

    add table

--- lualib.lua
+++ lualib.lua
@@ -12,7 +12,7 @@ math.asin = function(x) return deg(asin(x)) end
 math.randomseed(os.time())
 --]]
 
-
+--[[
 t = {
     
 }
@@ -20,3 +20,72 @@ for line in io.lines do
     table.insert(t,line)
 end
 print(#t)
+--]]
+
+
+
+--[[
+lines = {
+    luaH_set = 10,
+    luaH_get = 24,
+    luaH_present = 48,
+}
+
+
+a = {
+
+}
+
+for n in pairs(lines) do a[#a +1] = n end
+table.sort(a)
+for i,n in ipairs(a) do print(n) end
+
+
+function pairsByKeys(t,f)
+    local a = {
+    }
+    for n in pairs(t) do a[#a+1] = n end
+    table.sort(a,f)
+    local i = 0
+    return function()
+        i = i+1 --]]
+--        return a[i],t[a[i]]
+--[[
+    end
+end
+
+
+for name,line in pairsByKeys(lines) do
+    print(name,line)
+end
+
+--]]
+
+
+function rconcat(l)
+    if type(l) ~= "table" then return l end
+    local res = {
+
+    }
+    for i =1,#l do
+        res[i] = rconcat(l[i])
+    end
+    return table.concat(res)
+end
+
+
+print(rconcat{
+    {
+        " a",{" nice"}
+    }," and",
+    {
+
+    
+    {
+        " long"
+    },
+    {
+        " list"
+    }
+}
+})
