949e3e0e5964677b2a3e8a35f280212b3cf1b783
samyunwei <259375702@qq.com>
2016-11-07 23:53:34 +0800

    add iolua

---
+++ iolua.lua
@@ -0,0 +1,8 @@
+#!/usr/bin/env lua
+--# File Name: iolua.lua
+--# Author: Sam
+--# mail: samyunwei@163.com
+--# Created Time: 2016年11月07日 星期一 23时39分11秒
+--#########################################################################
+io.write("sin(3) = ",math.sin(3),"\n")
+io.write(string.format("sin (3) = %.4f\n",math.sin(3)))
