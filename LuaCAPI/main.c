757e9c6678c9fff44272e922b40450edec75db82
samyunwei <259375702@qq.com>
2016-11-28 18:54:26 +0800

    push

--- LuaCAPI/main.c
+++ LuaCAPI/main.c
@@ -7,17 +7,55 @@
 
 #include <stdio.h>
 #include <lua.h>
-#include <luaxlib.h>
+#include <lauxlib.h>
 #include <lualib.h>
 #include <math.h>
+#include <dirent.h>
+#include <errno.h>
+
 static int l_sin(lua_State *L)
 {
-    double d = lua_tonumber(L,1);
+    double d = luaL_checknumber(L,1);
     lua_pushnumber(L,sin(d));
-    return 1
+    return 1;
 }
-int main(void)
+
+static int l_dir(lua_State *L)
 {
-    printf("%s","hello world!");
-    return 0;
+    DIR *dir;
+    struct dirent *entry;
+    int i;
+    const char *path = luaL_checkstring(L,1);
+    dir = opendir(path);
+    if(dir == NULL)
+    {
+        lua_pushnil(L);
+        lua_pushstring(L,strerror(errno));
+        return 2;
+    }
+
+    lua_newtable(L);
+    i = 1;
+    while((entry = readdir(dir)) != NULL)
+    {
+        lua_pushnumber(L,i++);
+        lua_pushstring(L,entry->d_name);
+        lua_settable(L,-3);
+    }
+
+    closedir(dir);
+    return 1;
+}
+
+static const struct luaL_Reg mylib[] = {
+    {"dir",l_dir},
+    {NULL,NULL}
+};
+
+int luaopen_mylib(lua_State *L)
+{
+    //luaL_register(L,"mylib",mylib);
+    lua_newtable(L);
+    luaL_setfuncs(L,mylib,0);
+    return 1;
 }
