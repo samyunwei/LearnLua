894d8a2326c8c08323aebcf4de38abb7ec9f358e
samyunwei <259375702@qq.com>
2016-12-02 00:03:18 +0800

    add Carray

--- LuaCAPI/LuaCAPI/LuaCAPI.h
+++ LuaCAPI/LuaCAPI/LuaCAPI.h
@@ -16,7 +16,7 @@ extern "C" {
 #include <ctype.h>
 #include <limits.h>
 #define BITS_PER_WORD  (CHAR_BIT*sizeof(unsigned int))
-#define I_WORD(i)  ((unsigned int)(i) / BITS_PER_WORD)
+#define I_WORD(i)  ((unsigned int) (i) / BITS_PER_WORD)
 #define I_BIT(i) (1 << ((unsigned int)(i) % BITS_PER_WORD))
 
 typedef struct NumArray{
@@ -190,6 +190,86 @@ int luaopen_mylib(lua_State *L)
 //    luaL_setfuncs(L,mylib,0);
 //    return 1;
 //}
+
+
+static int newarray(lua_State *L)
+{
+    int i,n;
+    size_t nbytes;
+    NumArray *a;
+
+    n = luaL_checkinteger(L,1);
+   luaL_argcheck(L,n >=1,1,"invalid size");
+   nbytes = sizeof(NumArray) + I_WORD(n-1) * sizeof(unsigned int);
+   a = (NumArray *)lua_newuserdata(L,nbytes);
+
+   a->size = n;
+   for(i = 0;i <= I_WORD(n-1);i++)
+   {
+       a->values[i] = 0;
+   }
+
+   return 1;
+}
+
+static int setarray(lua_State *L)
+{
+    NumArray *a = (NumArray *) lua_touserdata(L,1);
+    int index = luaL_checkinteger(L,2) -1;
+    luaL_checkany(L,3);
+
+    luaL_argcheck(L,a != NULL,1," 'array' expected ");
+
+    luaL_argcheck(L,0 <= index && index < a->size,2,"index out of range");
+
+    if(lua_toboolean(L,3))
+    {
+        a->values[I_WORD(index)] |= I_BIT(index);
+    }else
+    {
+        a->values[I_WORD(index)] &= ~I_BIT(index);
+    }
+    return 0;
+}
+
+
+static int getarray(lua_State *L)
+{
+    NumArray *a = (NumArray*)lua_touserdata(L,1);
+    int index = luaL_checkinteger(L,2) - 1;
+
+    luaL_argcheck(L,a != NULL,1," 'array' expected");
+
+    luaL_argcheck(L,0 <= index && index <a->size,2,"index out of range");
+    lua_pushboolean(L,a->values[I_WORD(index)] & I_BIT(index));
+    return 1;
+}
+
+
+static int getsize(lua_State *L)
+{
+    NumArray *a = (NumArray*)lua_touserdata(L,1);
+    luaL_argcheck(L, a != NULL,1," ' array' expected");
+    lua_pushinteger(L,a->size);
+    return 1;
+}
+
+static const struct luaL_Reg mylib[] = {
+    {"new",newarray},
+    {"set",setarray },
+    {"get",getarray},
+    {"size",getsize},
+    {NULL,NULL}
+};
+
+int luaopen_mylib(lua_State *L)
+{
+    lua_newtable(L);
+    luaL_setfuncs(L,mylib,0);
+    return 1;
+}
+
+
 #ifdef __cplusplus
 }
 #endif
---
+++ LuaCAPI/testluaCarry.lua
@@ -0,0 +1,18 @@
+#!/usr/bin/env lua
+--# File Name: testluaCarry.lua
+--# Author: Sam
+--# mail: samyunwei@163.com
+--# Created Time: 2016年12月01日 星期四 23时59分08秒
+--#########################################################################
+
+array = require "mylib"
+
+a = array.new(1000)
+print(a)
+print(array.size(a))
+
+for i = 1,1000 do
+    array.set(a,i, i%5 == 0)
+end
+
+print(array.get(a,10))
