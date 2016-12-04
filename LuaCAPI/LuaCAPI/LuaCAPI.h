e9468951c565c7c3b6c228e6db365bcdde7d89e3
samyunwei <259375702@qq.com>
2016-12-05 00:30:21 +0800

    add userdatas

--- LuaCAPI/LuaCAPI/LuaCAPI.h
+++ LuaCAPI/LuaCAPI/LuaCAPI.h
@@ -18,7 +18,8 @@ extern "C" {
 #define BITS_PER_WORD  (CHAR_BIT*sizeof(unsigned int))
 #define I_WORD(i)  ((unsigned int) (i) / BITS_PER_WORD)
 #define I_BIT(i) (1 << ((unsigned int)(i) % BITS_PER_WORD))
-
+#define checkarrary(L) \
+    (NumArray*)luaL_checkudata(L,1,"LuaBook.array")
 typedef struct NumArray{
     int size;
     unsigned int values[1];
@@ -209,25 +210,63 @@ static int newarray(lua_State *L)
        a->values[i] = 0;
    }
 
+   luaL_getmetatable(L,"LuaBook.array");
+   lua_setmetatable(L,-2);
    return 1;
 }
 
+static unsigned int *getindex(lua_State *L,unsigned int* mask)
+{
+    NumArray* a = checkarrary(L);
+    int index = luaL_checkinteger(L,2) - 1;
+     luaL_argcheck(L,0 <= index && index < a->size,2,"index out of range");
+     *mask = I_BIT(index);
+     return &a->values[I_WORD(index)];
+}
+
+//static int setarray(lua_State *L)
+//{
+//    NumArray *a = (NumArray *) lua_touserdata(L,1);
+//    int index = luaL_checkinteger(L,2) -1;
+//    luaL_checkany(L,3);
+
+//    luaL_argcheck(L,a != NULL,1," 'array' expected ");
+
+//    luaL_argcheck(L,0 <= index && index < a->size,2,"index out of range");
+
+//    if(lua_toboolean(L,3))
+//    {
+//        a->values[I_WORD(index)] |= I_BIT(index);
+//    }else
+//    {
+//        a->values[I_WORD(index)] &= ~I_BIT(index);
+//    }
+//    return 0;
+//}
+
+//static int getarray(lua_State *L)
+//{
+//    NumArray *a = (NumArray*)lua_touserdata(L,1);
+//    int index = luaL_checkinteger(L,2) - 1;
+
+//    luaL_argcheck(L,a != NULL,1," 'array' expected");
+
+//    luaL_argcheck(L,0 <= index && index <a->size,2,"index out of range");
+//    lua_pushboolean(L,a->values[I_WORD(index)] & I_BIT(index));
+//    return 1;
+//}
+
 static int setarray(lua_State *L)
 {
-    NumArray *a = (NumArray *) lua_touserdata(L,1);
-    int index = luaL_checkinteger(L,2) -1;
+    unsigned int mask;
+    unsigned int *entry = getindex(L,&mask);
     luaL_checkany(L,3);
-
-    luaL_argcheck(L,a != NULL,1," 'array' expected ");
-
-    luaL_argcheck(L,0 <= index && index < a->size,2,"index out of range");
-
     if(lua_toboolean(L,3))
     {
-        a->values[I_WORD(index)] |= I_BIT(index);
+        *entry |= mask;
     }else
     {
-        a->values[I_WORD(index)] &= ~I_BIT(index);
+        *entry &= ~mask;
     }
     return 0;
 }
@@ -235,37 +274,58 @@ static int setarray(lua_State *L)
 
 static int getarray(lua_State *L)
 {
-    NumArray *a = (NumArray*)lua_touserdata(L,1);
-    int index = luaL_checkinteger(L,2) - 1;
-
-    luaL_argcheck(L,a != NULL,1," 'array' expected");
-
-    luaL_argcheck(L,0 <= index && index <a->size,2,"index out of range");
-    lua_pushboolean(L,a->values[I_WORD(index)] & I_BIT(index));
+    unsigned int mask;
+    unsigned int *entry = getindex(L,&mask);
+    lua_pushboolean(L,*entry & mask);
     return 1;
 }
+
 
 
 static int getsize(lua_State *L)
 {
-    NumArray *a = (NumArray*)lua_touserdata(L,1);
-    luaL_argcheck(L, a != NULL,1," ' array' expected");
+//    NumArray *a = (NumArray*)lua_touserdata(L,1);
+//    luaL_argcheck(L, a != NULL,1," ' array' expected");
+    NumArray* a= checkarrary(L);
     lua_pushinteger(L,a->size);
     return 1;
 }
+int array2string(lua_State *L)
+{
+    NumArray* a = checkarrary(L);
+    lua_pushfstring(L,"array(%d)",a->size);
+    return 1;
+}
 
-static const struct luaL_Reg mylib[] = {
+static const struct luaL_Reg arrylib_f[] = {
     {"new",newarray},
-    {"set",setarray },
-    {"get",getarray},
-    {"size",getsize},
+    {NULL,NULL}
+};
+
+//static const struct luaL_Reg arrylib_m[] = {
+//    {"set",setarray },
+//    {"get",getarray},
+//    {"size",getsize},
+//    {"__tostring",array2string},
+//    {NULL,NULL}
+//};
+
+static const struct luaL_Reg arrylib_m[] = {
+    {"__newindex",setarray },
+    {"__index",getarray},
+    {"__len",getsize},
+    {"__tostring",array2string},
     {NULL,NULL}
 };
 
 int luaopen_mylib(lua_State *L)
 {
+    luaL_newmetatable(L,"LuaBook.array");
+    lua_pushvalue(L,-1);
+    lua_setfield(L,-1,"__index");
+    luaL_setfuncs(L,arrylib_m,0);
     lua_newtable(L);
-    luaL_setfuncs(L,mylib,0);
+    luaL_setfuncs(L,arrylib_f,0);
     return 1;
 }
 
--- LuaCAPI/testluaCarry.lua
+++ LuaCAPI/testluaCarry.lua
@@ -8,6 +8,7 @@
 array = require "mylib"
 
 a = array.new(1000)
+--[[
 print(a)
 print(array.size(a))
 
@@ -16,3 +17,12 @@ for i = 1,1000 do
 end
 
 print(array.get(a,10))
+--array.get(io.stdin,10)
+
+print(a)
+print(a:size())
+--]]
+
+a[10] = true
+print(a[10])
+print(#a)
